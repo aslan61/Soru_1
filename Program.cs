577201fc19a7d45bd31a27c80be7b339f14ac7b8
Ahmet Aslan <simsar6155@gmail.com>
2022-07-22 22:04:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Soru_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru1: Kullanıcının girdiği sayının %18 ini bulup yazdıran basit C# Console örneği.
+            double sonuç = 0;
+            Console.WriteLine("Yüzdesi Alınacak Sayı: ");
+            double sayı = Convert.ToInt16(Console.ReadLine());
+            sonuç=sayı*(0.18);
+            Console.WriteLine("Sayının Yüzde onsekizi: {0}",sonuç);
+        }
+    }
+}
